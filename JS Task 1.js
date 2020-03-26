9bfa90847ccf9e0ed17036c77bbc9c35849736ec
imohetta <imohettafr@gmail.com>
2020-03-26 04:06:59 +0100

    add existing file

---
+++ JS Task 1.js
@@ -0,0 +1,20 @@
+
+let name = "Imoh Etta";
+
+let courses = ["HTML", "CSS", "JS", "C#", "UX/UI"];
+console.log("My name is:" + name + " " + courses)
+
+
+const data = [];
+
+for (let i=0; i<=200; i++) {
+
+  if (i % 2 != 0) {
+
+    data.push(i);
+
+  }
+
+}
+
+console.log(data);
